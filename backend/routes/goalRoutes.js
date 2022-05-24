5c483a6bf7903b2001af0d37f9d0945bbfd1869c
Nikola <nikola94lukic@hotmail.com>
2022-05-24 23:12:43 +0200

    CRUD API with Auth finished

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -2,16 +2,10 @@ const express = require('express')
 const { route } = require('express/lib/application')
 const router = express.Router()
 const {getGoals, setGoals, updateGoals,  deleteGoals} = require('../controllers/goalController')
+const {protect} = require('../middleware/authMiddleware')
 
-router.route('/').get(getGoals).post(setGoals)
+router.route('/').get(protect, getGoals).post(protect, setGoals)
 
-// router.get('/', getGoals)
-// router.post('/', setGoals)
-
-router.route('/:id').put(updateGoals).delete(deleteGoals)
-// router.put('/:id', updateGoals)
-// router.delete('/:id', deleteGoals)
-
-
+router.route('/:id').put(protect, updateGoals).delete(protect, deleteGoals)
 
 module.exports = router
