67704c9bbc1960de8148d1add7cf24af5ca686fa
Nikola <nikola94lukic@hotmail.com>
2022-05-30 20:00:26 +0200

    FE Auth

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,5 +1,9 @@
 import {useState, useEffect} from 'react'
-import {FaSignInAlt} from 'react-icons/fa'
+import {useSelector, useDispatch} from 'react-redux'
+import {useNavigate} from 'react-router-dom'
+import {toast} from 'react-toastify'
+import {login, reset} from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
 function Login() {
     const [formData, setFormData] = useState({
@@ -9,6 +13,13 @@ function Login() {
 
     const {email, password} = formData
 
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const {user, isLoading, isError, isSuccess, message} = useSelector((state) => {
+        state.auth
+    })
+
     const onChange = (e) => {
             setFormData((prevState) => ({
                 ...prevState,
@@ -16,8 +27,31 @@ function Login() {
             }))
     }
 
+    useEffect(() => {
+        if (isError) {
+            toast.error(message)
+        }
+
+        if (isSuccess || user) {
+            navigate('/')
+        }
+
+        dispatch(reset)
+
+    }, [user, isError, isSuccess, message, navigate, dispatch])
+
     const onSubmit = (e) => {
         e.preventDefault()
+
+        const userData = {
+            email, password
+        }
+
+        dispatch(login(userData))
+    }
+
+    if (isLoading) {
+        return <Spinner />
     }
 
   return (
