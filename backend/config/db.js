61cb12740c9c23d502ae0419ae1476a8b0e2899f
Nikola <nikola94lukic@hotmail.com>
2022-06-05 09:59:05 -0600

    Bugfix & refactoring entire project

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose')
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
-        console.log(`Mongo DB connected: ${conn.connection.host}`).cyan.underline;
-    } catch (error) {
-        console.log(error)
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI)
 
-        process.exit(1)
-    }
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
 }
 
 module.exports = connectDB
